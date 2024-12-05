e65f5a76827e386d5b828c06dfdbd0b392f3e9a9
David Simpson <david@gtedge.ai>
2024-12-04 18:33:14 -0600

    Delete src directory

--- src/main/java/com/taskmanager/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taskmanager.repository;
-
-import com.taskmanager.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
