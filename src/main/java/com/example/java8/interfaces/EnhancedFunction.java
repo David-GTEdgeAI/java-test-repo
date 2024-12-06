3b728c62bd79c5c71fd660ba59ba5037b7351078
David Simpson <david@gtedge.ai>
2024-12-05 18:33:02 -0600

    [Java 17] Update src/main/java/com/example/java8/interfaces/EnhancedFunction.java from Jira ticket JAVA-157
    - Timestamp: 2024-12-05 18:33:01
    - Source: Jira code review
    - Ticket: JAVA-157
    - File: src/main/java/com/example/java8/interfaces/EnhancedFunction.java
    - Branch: testing
    - Type: Java 17 conversion

---
+++ src/main/java/com/example/java8/interfaces/EnhancedFunction.java
@@ -0,0 +1,22 @@
+package com.example.java8.interfaces;
+
+import java.util.function.Predicate;
+import java.util.function.Function;
+
+public interface EnhancedFunction<T, R> extends Function<T, R> {
+    static <T> EnhancedFunction<T, T> identity() {
+        return t -> t;
+    }
+
+    default <V> EnhancedFunction<T, V> andThenEnhanced(EnhancedFunction<R, V> after) {
+        return (T t) -> after.apply(this.apply(t));
+    }
+
+    default EnhancedFunction<T, R> filter(Predicate<? super T> predicate) {
+        return t -> predicate.test(t) ? apply(t) : null;
+    }
+
+    default <V> EnhancedFunction<V, R> compose(Function<? super V, ? extends T> before) {
+        return (V v) -> apply(before.apply(v));
+    }
+}
